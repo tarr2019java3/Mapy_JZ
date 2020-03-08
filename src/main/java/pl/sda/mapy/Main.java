804db38d5c251d4d93c8f587975abd7308f415c8
kubaZ <zadrozinskijakub@gmail.com>
2020-03-08 10:56:45 +0100

    mapy

--- src/main/java/pl/sda/mapy/Main.java
+++ src/main/java/pl/sda/mapy/Main.java
@@ -3,6 +3,10 @@ package pl.sda.mapy;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+Generyka<String> generyka = new Generyka<String>("Test");
+        System.out.println(generyka.getField());
+
+        Generyka<Integer> generyka1 = new Generyka<Integer>(23233);
+        System.out.println(generyka1.getField());
     }
 }
